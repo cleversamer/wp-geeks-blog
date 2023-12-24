1b644ec4233d90f832e65b6d4bbe0d69bb773b0a
cleversamerr <thedev.samer@gmail.com>
2023-12-24 08:41:04 +0200

    updated header and footer

--- themes/geeks-blog/footer.php
+++ themes/geeks-blog/footer.php
@@ -1,3 +1,9 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+    <p class="copyright"><a href="https://github.com/cleversamerr">cleversamer</a></p>
+</footer>
+
+</div>
+
 <?php wp_footer(); ?>
 
 </body>
--- themes/geeks-blog/header.php
+++ themes/geeks-blog/header.php
@@ -59,3 +59,8 @@
             </div>
         </nav>
     </header>
+
+    <div class="main-wrapper">
+        <header class="page-title theme-bg-light text-center gradient py-5">
+            <h1 class="heading">Blog Home Page Heading</h1>
+        </header>
